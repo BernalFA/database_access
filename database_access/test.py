67f8a0644ec2892522f7c67bdfb9c50c94b8f160
BernalFA <fabernal1230@gmail.com>
2025-02-25 17:10:23 +0100

    test notebook to script

---
+++ database_access/test.py
@@ -0,0 +1,27 @@
+"""
+Short test of the search_compounds function.
+
+@author: Dr. Freddy A. Bernal
+"""
+
+import os
+import pandas as pd
+from connect import search_compounds
+
+# SQL expression
+sql = """SELECT CM.CM_UNITS.CMU_REGID,QUATTRO_CR.BATCH.BATCH_REGID,
+QUATTRO_CR.MOLECULE.MOL_REGID,QUATTRO_CR.MOLECULE.MOL_CTFILE
+FROM CM.CM_UNITS,QUATTRO_CR.BATCH,QUATTRO_CR.MOLECULE
+WHERE CM.CM_UNITS.CMA_ID = QUATTRO_CR.BATCH.BATCH_ID
+AND QUATTRO_CR.BATCH.MOL_COMP_ID = QUATTRO_CR.MOLECULE.MOL_COMP_ID
+AND QUATTRO_CR.BATCH.BATCH_REGID = :mybv"""
+
+path = os.path.abspath(os.getcwd())
+# get list of compounds
+df = pd.read_excel(os.path.join(path, "test", "test_data.xlsx"))
+print(f"Source dataframe shape: {df.shape}")
+
+# query database
+result = search_compounds(df["Batch ID"], sql=sql)
+print(f"Resulting dataframe shape: {result.shape}")
+print(result.head(3))
