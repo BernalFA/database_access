083524c0e2665c1a861967e1a8b9711d87bccd92
BernalFA <fabernal1230@gmail.com>
2025-03-04 17:03:18 +0100

    restructure tests

--- test/test_warning.py
+++ test/test_warning.py
@@ -6,7 +6,7 @@ Short test of the search_compounds function.
 
 import os
 import pandas as pd
-from connect import search_compounds
+from database_access import search_compounds
 
 # SQL expression
 sql = """SELECT CM.CM_UNITS.CMU_REGID,QUATTRO_CR.BATCH.BATCH_REGID,
@@ -14,14 +14,23 @@ QUATTRO_CR.MOLECULE.MOL_REGID,QUATTRO_CR.MOLECULE.MOL_CTFILE
 FROM CM.CM_UNITS,QUATTRO_CR.BATCH,QUATTRO_CR.MOLECULE
 WHERE CM.CM_UNITS.CMA_ID = QUATTRO_CR.BATCH.BATCH_ID
 AND QUATTRO_CR.BATCH.MOL_COMP_ID = QUATTRO_CR.MOLECULE.MOL_COMP_ID
-AND QUATTRO_CR.BATCH.BATCH_REGID = :mybv"""
+AND QUATTRO_CR.MOLECULE.MOL_REGID = :mybv"""
 
 path = os.path.abspath(os.getcwd())
 # get list of compounds
-df = pd.read_excel(os.path.join(path, "test", "test_data.xlsx"))
+df = pd.read_excel(os.path.join(path, "test", "test_data_medium.xlsx"))
 print(f"Source dataframe shape: {df.shape}")
 
 # query database
-result = search_compounds(df["Batch ID"], sql=sql)
-print(f"Resulting dataframe shape: {result.shape}")
-print(result.head(3))
+result = search_compounds(df["Compound_Id"], sql=sql)
+print(f"Query results: {len(result)}")
+print(result.iloc[[10, 500, 927]])
+
+# # Transform CT files
+# ct_strings = [x[-1] for x in result]
+# mols, messages = transform_ct(ct_strings)
+# issues = 0
+# for i, msn in enumerate(messages):
+#     if msn != "":
+#         issues += 1
+# print(f"Number of warnings: {issues} / {len(messages)}")
